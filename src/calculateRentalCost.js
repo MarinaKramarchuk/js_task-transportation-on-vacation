f2406620860ed8f32b7a30690a9854ad019926ff
Marina Kramarchuk <kramarchukmm20182018@gmail.com>
2025-09-09 14:59:55 +0300

    fixed variables

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,19 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const DAY_RATE = 40;
+  const DAILY_RATE = 40;
   const LONG_TERM_DAYS = 7;
   const LONG_TERM_DISCOUNT = 50;
-  const MID_TERM_DAYS = 3;
-  const MID_TERM_DISCOUNT = 20;
-  const basePrice = DAY_RATE * days;
+  const MEDIUM_TERM_DAYS = 3;
+  const MEDIUM_TERM_DISCOUNT = 20;
+  const basePrice = DAILY_RATE * days;
 
   if (days >= LONG_TERM_DAYS) {
     return basePrice - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= MID_TERM_DAYS) {
-    return basePrice - MID_TERM_DISCOUNT;
+  if (days >= MEDIUM_TERM_DAYS) {
+    return basePrice - MEDIUM_TERM_DISCOUNT;
   }
 
   return basePrice;
