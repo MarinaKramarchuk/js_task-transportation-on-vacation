7014fe1417d438424bb3b027a0861dd4ff2022b3
Marina Kramarchuk <kramarchukmm20182018@gmail.com>
2025-09-09 13:47:41 +0300

    solution of hometask

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,24 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const DAY_COST = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCONT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCONT = 20;
+  const price = DAY_COST * days;
+
+  if (days < SHORT_TERM) {
+    return price;
+  }
+
+  if (days < LONG_TERM) {
+    return price - SHORT_TERM_DISCONT;
+  }
+
+  if (days >= LONG_TERM) {
+    return price - LONG_TERM_DISCONT;
+  }
 }
 
 module.exports = calculateRentalCost;
